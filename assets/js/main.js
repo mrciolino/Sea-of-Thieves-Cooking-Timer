b42f7136d9a8799696895adda175064f17a48fcf
Matt <mrciolino@comcast.net>
2021-09-15 15:26:49 -0400

    making button out of food names so ppl know to click

--- assets/js/main.js
+++ assets/js/main.js
@@ -128,6 +128,41 @@ for (let food in food_cooking_times_sea_of_thieves) {
     $('#' + food).single_double_click(single, double);
 };
 
+(function($) {
+    $.fn.nodoubletapzoom = function() {
+        $(this).bind('touchstart', function preventZoom(e) {
+            var t2 = e.timeStamp,
+                t1 = $(this).data('lastTouch') || t2,
+                dt = t2 - t1,
+                fingers = e.originalEvent.touches.length;
+            $(this).data('lastTouch', t2);
+            if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+            e.preventDefault(); // double tap - prevent the zoom
+            // also synthesize click events we just swallowed up
+            $(this).trigger('click').trigger('click');
+        });
+    };
+})(jQuery);
+
+(function($) {
+    var IS_IOS = /iphone|ipad/i.test(navigator.userAgent);
+    $.fn.nodoubletapzoom = function() {
+        if (IS_IOS)
+            $(this).bind('touchstart', function preventZoom(e) {
+                var t2 = e.timeStamp,
+                    t1 = $(this).data('lastTouch') || t2,
+                    dt = t2 - t1,
+                    fingers = e.originalEvent.touches.length;
+                $(this).data('lastTouch', t2);
+                if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+                e.preventDefault(); // double tap - prevent the zoom
+                // also synthesize click events we just swallowed up
+                $(this).trigger('click').trigger('click');
+            });
+    };
+})(jQuery);
 
 /**
  * Template Name: OnePage - v4.3.0
