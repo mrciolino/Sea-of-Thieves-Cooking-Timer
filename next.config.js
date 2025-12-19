755f3a4e61847465de7159cf8cde88444c5dfbd7
Matthew Ciolino <mrciolino@comcast.net>
2025-12-19 16:55:33 -0500

    feat: update next.config.js to include output and images configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Generate a static export to the `out` directory
+  output: 'export',
+  // If `next/image` is used later, avoid server-side optimization
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
